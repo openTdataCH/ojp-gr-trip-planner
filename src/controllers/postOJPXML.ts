16ad356e3475d279077eab4d6d1384bdaf7d9417
sido114 <sido11412@gmail.com>
2023-03-09 15:30:42 +0100

    CH trips are working - Fuck yeah!

--- src/controllers/postOJPXML.ts
+++ src/controllers/postOJPXML.ts
@@ -2,7 +2,7 @@
 import { Request, Response } from 'express';
 import * as xml2js from 'xml2js';
 import jsdom from 'jsdom';
-import { Location } from 'ojp-sdk';
+import OJP from 'ojp-sdk';
 import {
   createLocationInfoResponse,
   NameToSystemMapper,
@@ -11,7 +11,10 @@ import {
 import { XMLRequest } from '../types/xmlRequest';
 import CONFIG from '../config';
 import { locationInformationRequest } from '../passiveSystems/locationInformationRequest';
-import { PASSIVE_SYSTEM } from '../config/passiveSystems';
+import { PASSIVE_SYSTEM, passiveSystemsConfig } from '../config/passiveSystems';
+import { createTripResponse } from '../helpers/createTripResponse';
+
+const { Location, TripLocationPoint, TripsRequestParams } = OJP;
 
 const { JSDOM } = jsdom;
 global.DOMParser = new JSDOM().window.DOMParser;
@@ -39,8 +42,32 @@ export const postOJPXML = async (req: Request, res: Response) => {
         );
       return res.status(200).send(resultXML);
     } catch (e) {
+      console.error(e);
       throw e;
     }
+  } else if (serviceRequest.requestType === 'TripRequest') {
+    const originRef = serviceRequest.body.origin;
+    const departTimeString = originRef.departTime;
+    const destinationRef = serviceRequest.body.destination.placeRef;
+    const destination = Location.initWithStopPlaceRef(
+      String(destinationRef.stopPointRef),
+      destinationRef.locationName,
+    );
+    const departTime = new Date(departTimeString);
+    const origin = Location.initWithStopPlaceRef(
+      String(originRef.placeRef.stopPointRef),
+      originRef.placeRef.locationName,
+    );
+    const tripRequestParams = TripsRequestParams.initWithLocationsAndDate(
+      new TripLocationPoint(origin),
+      new TripLocationPoint(destination),
+      departTime,
+    );
+    const tripRequest = new OJP.TripRequest(
+      passiveSystemsConfig.SBB,
+      tripRequestParams!,
+    );
+    return res.status(200).send(await getTripRespons(tripRequest));
   }
 
   const builder = new xml2js.Builder();
@@ -50,7 +77,7 @@ export const postOJPXML = async (req: Request, res: Response) => {
   return res.status(200).send(xml);
 };
 
-function makeDistinctLocations(locations: Location[]) {
+function makeDistinctLocations(locations: OJP.Location[]) {
   return [
     ...new Map(
       locations.map(location => [location.computeLocationName(), location]),
@@ -61,16 +88,40 @@ function makeDistinctLocations(locations: Location[]) {
 async function createLocationInformationResponseFromPassiveSystems(
   initialInput: string,
 ) {
-  const responseFromPassiveSystems = await Promise.all(
-    Object.values(CONFIG.PASSIVE_SYSTEMS).map(async passiveSystem => {
-      return (
-        await locationInformationRequest(passiveSystem, initialInput)
-      ).map(location =>
-        NameToSystemMapper.add(location, passiveSystem.key as PASSIVE_SYSTEM),
-      );
-    }),
-  );
-  return createLocationInfoResponse(
-    makeDistinctLocations(responseFromPassiveSystems.flat()),
-  );
+  try {
+    const responseFromPassiveSystems = await Promise.all(
+      Object.values(CONFIG.PASSIVE_SYSTEMS).map(async passiveSystem => {
+        return (
+          await locationInformationRequest(passiveSystem, initialInput)
+        ).map(location => {
+          return NameToSystemMapper.add(
+            location,
+            passiveSystem.key as PASSIVE_SYSTEM,
+          );
+        });
+      }),
+    );
+    return createLocationInfoResponse(
+      makeDistinctLocations(responseFromPassiveSystems.flat()),
+    );
+  } catch (e) {
+    console.error(e);
+  }
+  return 'error';
+}
+
+async function getTripRespons(tripRequest: OJP.TripRequest): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    tripRequest.fetchResponse((responseText, errorData) => {
+      if (errorData) {
+        console.error(errorData);
+      }
+      try {
+        resolve(createTripResponse(responseText));
+      } catch (e) {
+        console.error(e);
+        reject(e);
+      }
+    });
+  });
 }
