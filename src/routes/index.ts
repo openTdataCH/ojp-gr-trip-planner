95b98e0cabe48cf1ddc538b2281eeae192062d0f
sido114 <sido11412@gmail.com>
2023-03-15 15:35:11 +0100

    Implemente healthCheck.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { postOJPXML } from '../controllers/postOJPXML';
+import { getHealthCheck } from '../controllers/healthCheck';
 
 const router = Router();
 
 router.post('/testXML', postOJPXML);
+router.get('/testXML/health', getHealthCheck);
 
 export default router;
